f5910bdc5ca1a6deed32d2a8eb47e6616218ca03
Edujvr <32462670+Edujvr@users.noreply.github.com>
2017-12-20 12:21:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.post("/webhook", (req, res, next) => {
   const action = req.body.result.action;
   switch(action) {   
     case 'control':
-
+//Envio de información a Chatbase libreria @google/chatbase
 	var msg = chatbase.newMessage('da9339a8-3149-4788-b348-8ddf5a3046a7', req.body.sessionId)
 	.setPlatform('Dialogflow') 
 	.setMessage(req.body.result.resolvedQuery) 
@@ -20,7 +20,7 @@ app.post("/webhook", (req, res, next) => {
 	.send()
 	.then(msg => console.log(msg.getCreateResponse()))
 	.catch(err => console.error(err));
-
+//Envio de información a Google Analytics libreria request
 	const url = 'https://www.google-analytics.com/collect?v=1&t=event&tid=UA-111480355-1&cid='+req.body.sessionId+'&dh=www.google-analytics.com&ec=Intento&ea='+req.body.result.metadata.intentName+'&el='+req.body.result.resolvedQuery+'&ev=1&aip=1';
      	var request = require('request');
 		request.get(encodeURI(url))
@@ -28,7 +28,7 @@ app.post("/webhook", (req, res, next) => {
           	if (err) throw err;
 	  	console.log('Successfully logged to GA , Response to Dialogflow');
         });
-		  
+//Envio de información webhook a Dialogflow		  
 	res.json({
             messages: req.body.result.fulfillment.messages,
             speech: req.body.result.fulfillment.speech,
